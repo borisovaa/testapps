15ebc31d0f5288bb3e750ee82ed07cba377e1368
borisovaa <borisov_aa@bk.ru>
2023-04-05 14:42:03 +0300

    Finish prepairing

---
+++ src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ClassicalMusic implements Music {
+    // ПРОВЕРКА РАБОТЫ ГИТ
+
+    @Override
+    public String getSong() {
+        return "Венгерская рапсодия";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-        System.out.println("Test-app");
-    }
-}
+public class Main {
+
+        public static void main(String[] args) {
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+            MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+            musicPlayer.playMusic();
+            System.out.println(musicPlayer.getName());
+            System.out.println(musicPlayer.getVolume());
+            context.close();
+        }
+ }
